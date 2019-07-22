f48d5d52f5a5c98c3a2413bf23d9437cc31207a4
Yidadaa <yidadaa@qq.com>
2019-07-22 21:59:49 +0800

    feat(new card): 完成新建卡片页

---
+++ src/pages/new-card/index.js
@@ -0,0 +1,32 @@
+Page({
+  data: {
+    isSelecting: false,
+    subItems: new Array(5).fill(0).map((v, i) => 'LeetCode题目精选' + i),
+    selectedIndex: 0
+  },
+
+  onListTap (e) {
+    this.setData({
+      isSelecting: true
+    })
+  },
+
+  onListItemTap (e) {
+    this.setData({
+      isSelecting: false,
+      selectedIndex: e.currentTarget.dataset.index
+    })
+  },
+
+  onCancelBtn (e) {
+    wx.showModal({
+      title: '提示',
+      content: '是否放弃编辑当前项目？',
+      success (res) {
+        if (res.confirm) {
+          wx.navigateBack()
+        }
+      }
+    })
+  }
+})
